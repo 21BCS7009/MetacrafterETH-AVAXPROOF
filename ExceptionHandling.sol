e9be6061f9ebf56d57025676f4ce5b5acc8b0e5b
21BCS7009 <136325048+21BCS7009@users.noreply.github.com>
2023-07-09 11:41:19 +0530

    ExceptionHandling.sol

--- ExceptionHandling.sol
+++ ExceptionHandling.sol
@@ -3,36 +3,35 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.18;
 
-contract ExceptionHandling {
-    uint public balance;
-    address owner;
+contract ExceptionHamdling{
+    
+    address public owner = msg.sender;
+    uint public age = 20;
 
-    constructor() {
-        owner = msg.sender;
+    // This is an example of require()
+    function requireFunction(uint x) public{
+        require(x!=0,"Age must be greater than 0");
+        age += x;
     }
 
-    function deposit(uint amount) public {
-        require(amount > 0, "Invalid deposit amount");
-        balance += amount;
-    }
-
-    function withdraw(uint amount) public {
-        require(amount > 0, "Invalid withdrawal amount");
-        require(balance >= amount, "Insufficient balance");
-        balance -= amount;
-    }
-
-    function assertExample() public pure returns (uint) {
-        uint a = 10;
-        uint b = 5;
-        assert(a > b);
-        return a - b;
-    }
-
-    function revertExample(uint x) public pure returns (uint) {
-        if (x == 0) {
-            revert("Cannot divide by zero");
+    // This is a function of revert()
+    function revertFunction(uint x) public{
+        age += x;
+        if(x<=0){
+            // revert throwError("Value should be greater than 0. Transaction is set to its initial state.",msg.sender);
+            revert("Value should be > 0");
         }
-        return 10 / x;
+    }
+
+    // This is a customize error for revert
+    error throwError(string,address);
+
+    // This is the function which describes use of assert
+    function checkOwner() public view{
+        assert(owner==0x5B38Da6a701c568545dCfcB03FcB875f56beddC4);
+    }
+
+    function getAge() public view returns (uint){
+        return age;
     }
 }
