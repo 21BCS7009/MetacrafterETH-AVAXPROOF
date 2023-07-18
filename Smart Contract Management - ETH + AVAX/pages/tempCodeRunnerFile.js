8d38f0a2c54ed994d29a6565c178cb1574c56778
21BCS7009 <136325048+21BCS7009@users.noreply.github.com>
2023-07-18 20:51:36 +0530

    Delete Smart Contract Management - ETH + AVAX directory

--- Smart Contract Management - ETH + AVAX/pages/tempCodeRunnerFile.js
+++
@@ -1,6 +0,0 @@
-const [ethWallet, setEthWallet] = useState(undefined);
-  const [account, setAccount] = useState(undefined);
-  const [atm, setATM] = useState(undefined);
-  const [balance, setBalance] = useState(undefined);
-  const [ownerName, setOwnerName] = useState(undefined);
-
